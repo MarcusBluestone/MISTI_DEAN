1c7116a12a89dae5dec051e4f038b3d64550402e
MarcusBluestone <mathwiz108@gmail.com>
2023-04-24 12:56:59 -0400

    Downloaded some example pdfs and started to work

--- Test.py
+++ Test.py
@@ -1,10 +1,20 @@
 from PyPDF2 import PdfReader
 
-reader = PdfReader("sample.pdf")
+reader = PdfReader("sample_blank2.pdf")
 number_of_pages = len(reader.pages)
-page = reader.pages[1]
+page = reader.pages[5]
 text = page.extract_text()
 
 print(number_of_pages)
-print(page)
-print(text)
+#print("Page", page)
+temp = text.split('\n')
+lines = [line[::-1] for line in temp]
+#print(lines[3])
+
+for line in lines:
+    print(line)
+
+
+Aleph = "א"
+print('\n' in text)
+Hebrew_Alphabet = {"א"}
