7afb59137aa8dd0885fb6dc6402a5efcdf283c09
MarcusBluestone <mathwiz108@gmail.com>
2023-04-24 12:43:03 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,10 @@
-import pdfminer
-print(pdfminer.__version__)
-text = extract_text('MISTI_DEAN/sample.pdf')
-print(text)
+from PyPDF2 import PdfReader
 
+reader = PdfReader("sample.pdf")
+number_of_pages = len(reader.pages)
+page = reader.pages[1]
+text = page.extract_text()
+
+print(number_of_pages)
+print(page)
+print(text)
