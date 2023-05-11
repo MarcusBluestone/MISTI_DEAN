fd7c4cef81b9ea9e51024ee76b8351c62c583ad6
MarcusBluestone <mathwiz108@gmail.com>
2023-05-11 14:00:37 -0400

    See Description
    Fixed Everything

--- Test.py
+++ Test.py
@@ -1,40 +1,40 @@
 from PyPDF2 import PdfReader
 
-#Getting The Actual Text
+#note: when printing Hebrew words, they will appear backwards. 
+#Leave them like this
 
-
-
-def format_text(file_name): #returns a bunch of fixed (reverse Hebrew reversal) pages
+def get_words(file_name): 
+    """
+    Converts a PDF Document into an ordered list of Hebrew
+    words that are found on the page.
+    """
     reader = PdfReader(file_name)
-   # number_of_pages = len(reader.pages)
+    total_list = []
     for page in reader.pages:
-        total_list = []
-        lines = [line[::-1] for line in temp]
-        yield [word[::-1] for word in page.extract_text().split(' ')]
-   # number_of_pages = len(reader.pages)
-   # page = reader.pages[14]
-  #  text = page.extract_text()
+        lines = [line for line in page.extract_text().split('\n')] #list of string lines
+        for line in lines:
+            for word in line.split(' '): #
+                if len(word) > 0:
+                    total_list.append(word)
+    return total_list
 
-full_text = format_text("sample_blank.pdf")
-print("here!")
-for p in full_text:
-    print(p)
-    break
-#print(number_of_pages)
-#print("Page", page)
-#temp = text.split('\n')
-#lines = [line[::-1] for line in temp]
-#full_text = ""
-#for line in lines:
-#    full_text += line
-
-##print(full_text)
+def find_all(word_list, target_word, exact_match = True):
+    """
+    Finds all instances of a word in the word_list. Exact_match 
+    determines if we're searching for an exact copy or a word 
+    that contains this target_word. 
+    """
+    all_instances = []
+    for index, word in enumerate(word_list):
+        if exact_match and word == target_word:
+            all_instances.append((index, word))
+        elif (not exact_match) and target_word in word:
+            all_instances.append((index, word))
+    return all_instances
 
 #Defining Language Stuff
-Aleph = "א"
 english = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-#print('\n' in text)
-Hebrew_Alphabet = {"א"}
+Hebrew_Alphabet = "אבבּגדהוזחטיככלמנסעפצקרשתןםךףץ"
 
 def is_english(word):
     for letter in word:
@@ -42,38 +42,44 @@ def is_english(word):
             return True
     return False
 
-#Getting all the citations
-all_citations = set()
-with open('All_Citations.rtf', 'r') as file:
-    large_doc_citation = file.read()
-    
-for line in large_doc_citation.split('\n'):
-    dash = line.find('–') 
-    if dash >= 0:
-        abbrev = line[0:dash]
-        if not is_english(abbrev):
-            abbrev = abbrev[::-1].strip()
-            all_citations.add(abbrev)
+def get_all_citations():
+    """
+    Gets all of the citations from the 
+    All_Citations.rtf file. Returns a set
+    of Hebrew words
+    """
+    all_citations = set()
 
-#Adding English citations (in reverse b/c English ir processed backwards)
-all_citations.add("ppuS") 
-all_citations.add("G.P")
+    with open('All_Citations.rtf', 'r') as file:
+        large_doc_citation = file.read()
 
-#print(all_citations)
+    for line in large_doc_citation.split('\n'):
+        #print(line, line.find('–'))
+        dash = line.find('–')
+        if dash == -1:
+            dash = line.find('-')
+        if dash >= 0:
+            abbrev = line[0:dash]
+            if not is_english(abbrev):
+                abbrev = abbrev.strip()
+                all_citations.add(abbrev)
+        else:
+            print(line)
 
+    #Adding English citations (in reverse b/c English ir processed backwards)
+    all_citations.add("Supp") 
+    all_citations.add("P.G")
+    return all_citations
 
-def get_all_locations(all_pages):
-    #Scanning the Text Document
-
-    for num, page in enumerate(all_pages):
-        text = page.extract_text()
-        for cit in all_citations:
-            if cit in text:
-                print("here", num+1, cit)
-                loc = text.find(cit)
-                print(text[loc-5:loc+5])
-
-        break
+def get_all_locations(file_name):
+    word_list = get_words(file_name)
+    all_citations = get_all_citations()
+    print("here")
+    for cit in all_citations:
+        show_citation = cit[::-1]
+        all_instances = find_all(word_list, cit, False)
+        if len(all_instances) > 0:
+            print(show_citation, all_instances)
     return
 
-#get_all_locations()
+get_all_locations("sample_blank.pdf")
